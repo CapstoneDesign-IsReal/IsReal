bc98f4783691935c2e8485ee454b4d7915cc47d6
RooEstop <eunho02212@gmail.com>
2025-11-12 15:41:14 +0900

    [Feat] Behavior Tree 추가
    비헤비어 트리 / 블랙보드 / TaskNode 추가하였습니다.
    TaskNode는 먼저 Chase부터 구현 중

---
+++ Source/IsReal/TChase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "TChase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ISREAL_API UTChase : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
