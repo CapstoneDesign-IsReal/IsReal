7d3998574139c315acfadb8bde4c64e7da26dafd
tjdwns1314 <tjdwns1314@gmail.com>
2026-01-27 19:52:38 +0900

    애니메이션 정리
    1. 플레이어 사망 처리
    2. 캐릭터 움직일 때 애니메이션 고치기
    3. 총줍는 모션, pistol 모션 만들기
    4. 권총 추가하기 + 에임오프셋 수정하고 라이플 권총 모션 수정하기

--- Source/IsReal/PlayerCharacter.cpp
+++ Source/IsReal/PlayerCharacter.cpp
@@ -17,7 +17,7 @@
 #include "Kismet/GameplayStatics.h"
 
 // Sets default values
-APlayerCharacter::APlayerCharacter() // �ʱ�ȭ �ϱ� 
+APlayerCharacter::APlayerCharacter()  
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -28,26 +28,13 @@ APlayerCharacter::APlayerCharacter() // �ʱ�ȭ �ϱ�
 	SpringArmComp->SetupAttachment(RootComponent);
 	SpringArmComp->TargetArmLength = 400;
 	SpringArmComp->bUsePawnControlRotation = true;
-	SpringArmComp->SocketOffset = FVector(0.f, 70.f, 50.f); // (X=�յ�, Y=�¿�, Z=���Ʒ�)
-	SpringArmComp->SetRelativeRotation(FRotator(-15.f, 0.f, 0.f)); // -15�� �����ٺ��� ����
+	SpringArmComp->SocketOffset = FVector(0.f, 70.f, 50.f); 
+	SpringArmComp->SetRelativeRotation(FRotator(-15.f, 0.f, 0.f)); 
 
 	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
 	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
 	CameraComp->bUsePawnControlRotation = false;
 
-	//ConstructorHelpers::FObjectFinder<UStaticMesh> rifleMesh(TEXT("/Game/Fab/Free_Gun_Packs/Meshes/AK47_Body.AK47_Body"));
-	//gunMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("GunMeshComp"));
-	////GunMeshComp��� �̸����� ���ο� SkeletalMeshComponent�� �����. 
-	//gunMeshComp->SetupAttachment(GetMesh());
-	//// �� �� �޽ø� ��ü �޽ÿ� �ٿ���   , GetMesh()�� ĳ������ �� ���̷�Ż �޽� (��������Ʈ���� �پ�����)
-
-	//if (rifleMesh.Succeeded())
-	//{
-	//	gunMeshComp->SetStaticMesh(rifleMesh.Object); //gunMeshComp�� AK47_Body �� ����
-	//	// �߰�
-	//	gunMeshComp->SetupAttachment(GetMesh(), TEXT("RifleSocket"));
-	//	//�� �޽ø� ĳ���� ��ü�� RifleSocket�̶�� ���� ���Ϻκп� �ٿ��� 
-	//}
 
 	Rifle1 = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("RifleMesh"));
 	Rifle1->SetupAttachment(GetMesh()); //여기까지하면 블루프린트에 생김 
@@ -65,11 +52,11 @@ APlayerCharacter::APlayerCharacter() // �ʱ�ȭ �ϱ�
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	auto pc = Cast<APlayerController>(Controller); //�÷��̾� ��Ʈ�ѷ� �������� 
-	if (pc) //�����ϸ� 
+	auto pc = Cast<APlayerController>(Controller); 
+	if (pc) 
 	{
-		pc->PlayerCameraManager->ViewPitchMin = -50.f; //@@@@@@@@@
-		pc->PlayerCameraManager->ViewPitchMax = 50.f; //@@@@@@@@
+		pc->PlayerCameraManager->ViewPitchMin = -50.f; 
+		pc->PlayerCameraManager->ViewPitchMax = 50.f; 
 
 		auto subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(pc->GetLocalPlayer());
 
@@ -80,12 +67,11 @@ void APlayerCharacter::BeginPlay()
 	}
 	if (NormalCrossHairWidgetClass) {
 		NormalCrossHairWidget = CreateWidget<UUserWidget>(GetController<APlayerController>(), NormalCrossHairWidgetClass);
-		//CrossHairWidgetClass�� BP_CrosshairWidget�̸� CrossHairWidget�� ��´�. 
-		// => �״ϱ� ������������Ʈ�� �÷��̾� ��������ƮŬ�������� ������ �ϴ� ������ ������ؼ�.
+		
 
-		if (NormalCrossHairWidget) //�׸��� �� ���� ��ü�� �����ϸ� (������ ĳ���� ��������Ʈ���� ���� ���ָ�) 
+		if (NormalCrossHairWidget) 
 		{
-			NormalCrossHairWidget->AddToViewport(); //���� ���̱� 
+			NormalCrossHairWidget->AddToViewport(); 
 		}
 	}
 	if (AimCrossHairWidgetClass) {
@@ -222,23 +208,23 @@ void APlayerCharacter::ToggleClock(const FInputActionValue& inputValue)
 void APlayerCharacter::DoAimStart()
 {
 	if (IsHasGun) {
-		// ī�޶� ���� (�þ� ������)
+		
 		if (CameraComp)
 		{
 			CameraComp->SetFieldOfView(AimFOV);
 		}
 
-		// �������� ī�޶� �Ÿ� ���̱�
+		
 		if (SpringArmComp)
 		{
 			SpringArmComp->TargetArmLength = AimArmLength;
 		}
 		isAiming = true;
 
-		if (AimCrossHairWidget) //�׸��� �� ���� ��ü�� �����ϸ� (������ ĳ���� ��������Ʈ���� ���� ���ָ�) 
+		if (AimCrossHairWidget) 
 		{
-			AimCrossHairWidget->AddToViewport(); //���� ���̱� 
-			NormalCrossHairWidget->RemoveFromParent(); //���� �����
+			AimCrossHairWidget->AddToViewport(); 
+			NormalCrossHairWidget->RemoveFromParent(); 
 		}
 	}
 }
@@ -246,30 +232,32 @@ void APlayerCharacter::DoAimStart()
 void APlayerCharacter::DoAimEnd()
 {
 	if (IsHasGun) {
-		// ī�޶� �ǵ�����
+		
 		if (CameraComp)
 		{
 			CameraComp->SetFieldOfView(DefaultFOV);
 		}
 
-		// �������� �Ÿ� ����
+		
 		if (SpringArmComp)
 		{
 			SpringArmComp->TargetArmLength = DefaultArmLength;
 		}
 		isAiming = false;
-		if (NormalCrossHairWidget) //�׸��� �� ���� ��ü�� �����ϸ� (������ ĳ���� ��������Ʈ���� ���� ���ָ�) 
+		if (NormalCrossHairWidget) 
 		{
-			NormalCrossHairWidget->AddToViewport(); //���� ���̱� 
-			AimCrossHairWidget->RemoveFromParent(); //���� �����
+			NormalCrossHairWidget->AddToViewport(); 
+			AimCrossHairWidget->RemoveFromParent(); 
 		}
 	}
 }
 
 void APlayerCharacter::DoShootingStart()
 {
+	if (!CurrentWeapon) return;
+	//if (CurrentWeapon->IsReloading()) return;
 	// 가지고 있는 무기에 따라 fire가 다르게 나감
-	if (CurrentWeapon && IsHasGun) {
+	if (IsHasGun) {
 		IsShooting = true;
 
 		switch (CurrentWeapon->GetWeaponType()) {
@@ -318,13 +306,24 @@ void APlayerCharacter::Reload(const FInputActionValue& inputValue)
 
 // getters and setters for PlayerHP
 float APlayerCharacter::GetPlayerHP() { return PlayerHP; }
-void APlayerCharacter::SetPlayerHP(float HP) { PlayerHP = HP; }
+
+void APlayerCharacter::SetPlayerHP(float HP) { 
+	PlayerHP = HP;
+	UE_LOG(LogTemp, Warning, TEXT("PlayerHP = %f"), PlayerHP);
+	if (PlayerHP <= 0) {
+		PlayerDie();
+	}
+}
+
+void APlayerCharacter::PlayerDie() {
+	IsDie = true;
+}
 
 void APlayerCharacter::PInteract(const FInputActionValue& inputValue) {
 	FVector Start = CameraComp->GetComponentLocation();
 	FVector End = Start + (CameraComp->GetForwardVector() * 500.f);
 
-	FHitResult HitResult; // ���� Ʈ���̽��� ������ ����
+	FHitResult HitResult; 
 	FCollisionQueryParams param;
 	param.AddIgnoredActor(this);
 
--- Source/IsReal/PlayerCharacter.h
+++ Source/IsReal/PlayerCharacter.h
@@ -25,14 +25,12 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	class UCameraComponent* CameraComp;
 
-
-	// ���� ������Ʈ 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = GunMesh)
 	class UStaticMeshComponent* gunMeshComp;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = GunMesh) // To use WeaponSystem
 		class UStaticMeshComponent* Rifle1;
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = GunMesh) // @@
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = GunMesh) 
 		class UStaticMeshComponent* Pistol1;
 
 	UFUNCTION(BlueprintCallable, Category = "Weapon")
@@ -44,7 +42,6 @@ public:
 
 
 protected:
-	// ������ �ڵ����� ȣ��
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
@@ -54,52 +51,45 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	// ������Ʈ��
 
-	// ��������
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	class USpringArmComponent* SpringArmComp;
 
 
-
-
-
-
-
-	// �� �� �� ũ�ν���� ���� 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
-	TSubclassOf<UUserWidget> AimCrossHairWidgetClass; //���� ��������Ʈ Ŭ������ ������ ��ü������ 
+	TSubclassOf<UUserWidget> AimCrossHairWidgetClass; 
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
-	UUserWidget* AimCrossHairWidget; //������ ȭ�鿡 ǥ�õ� ���� �ν��Ͻ�(��ü)�� ������ ��ü���� 
+	UUserWidget* AimCrossHairWidget; 
 
 
-	// ���� ũ�ν� ��� ���� 
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
 	TSubclassOf<UUserWidget> NormalCrossHairWidgetClass;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
 	UUserWidget* NormalCrossHairWidget;
 
 
-	// �ð� UI
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
 	TSubclassOf<UUserWidget> ClockWidgetClass;
-	// �ð� UI �ν��Ͻ�
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
 	UUserWidget* ClockWidgetInstance;
-	// �ǰ��� VFX
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VFX")
 	UNiagaraSystem* RewindVFX;
 
 
 
-	// ��ǲ ���� ���� 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")// ��ǲ�������ؽ�Ʈ
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
 		class UInputMappingContext* imc_TPS;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")// T (�ð� �̵��ϱ�)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
 		class UInputAction* ia_Rewind;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")// TabŰ (�ð����� ������)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
 		class UInputAction* ia_ToggleClock;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input") // R (재장전)
@@ -108,87 +98,91 @@ protected:
 	UPROPERTY(EditDefaultsOnly, Category = "Input")
 	class UInputAction* ia_Interact;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")// ������ ���콺 (����)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
 		class UInputAction* AimAction;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")// ���� ���콺 (�ѽ��)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
 		class UInputAction* ShootingAction;
 
 
 
-	// ��ǲ ���ο� ���� ���� �� �Լ� 
 
 
-	void Rewind(const struct FInputActionValue& inputValue); //T ������ �� ����� 
+
+	void Rewind(const struct FInputActionValue& inputValue); 
 
 	void PInteract(const struct FInputActionValue& inputValue);
 
-	void ToggleClock(const FInputActionValue& Value); //�߰���
+	void ToggleClock(const FInputActionValue& Value); 
 
 	void Reload(const struct FInputActionValue& inputValue); // R 눌렀을 때 실행됨
 
-	virtual void DoAimStart();// ������ ���콺 ������ �� �����
+	virtual void DoAimStart();
 
-	virtual void DoAimEnd();// ������ ���� �� �����
+	virtual void DoAimEnd();
 
-	virtual void DoShootingStart();// ���� ���콺 ������ �� �����
+	virtual void DoShootingStart();
 
-	virtual void DoShootingEnd();// ���� ���콺 ���� �� �����
+	virtual void DoShootingEnd();
 
 
 
-	// ���߿� ó���ؾ��� 
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Aim")
-	bool IsHasGun = false; // ���� ��� �ִ���.
+	bool IsHasGun = false; 
+
+
+
+
 	// weapon system
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
 	AWeaponSystem* CurrentWeapon;
 
 
-	// ���� ���� 
+
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
-	float DefaultFOV = 90.f; //���� �þ߰�
+	float DefaultFOV = 90.f; 
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
-	float AimFOV = 65.f; // ���� �þ߰� 
+	float AimFOV = 65.f; 
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
-	float DefaultArmLength = 400.f; //���� ī�޶���� �Ÿ�
+	float DefaultArmLength = 400.f; 
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
-	float AimArmLength = 200.f; // ���ؽ� ī�޶���� �Ÿ� 
+	float AimArmLength = 200.f; 
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Aim")
-	bool isAiming = false; // ������ �ϰ� �ִ��� 
+	bool isAiming = false; 
 
 
 
-	// ���콺 ���ʹ�ư���� �� ��� ����
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
-	FTimerHandle AutoFireTimer; // �� ���� ��� ������ Ÿ�̸� �ڵ鷯 
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
-	bool IsShooting = false; //�� ��� �ִ���
+	FTimerHandle AutoFireTimer;  
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
-	float FireRange = 3000.f; // �� ������ �Ÿ� 
+	bool IsShooting = false; 
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+	float FireRange = 3000.f;
 
 
-	//�ð� �ٲ� �� ���� 
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = PlayerSetting)
 	bool Is_Rewind = false;
 
-	//tab�� ������ �ִ��� (�ð踦 ���� �ִ���)
-	UPROPERTY(EditAnywhere, BluePrintReadWrite) //�߰���
-		bool IsLookTimer = false;
+	
+	UPROPERTY(EditAnywhere, BluePrintReadWrite) 
+	bool IsLookTimer = false;
 
 	//Player Current HP
 	UPROPERTY(EditAnywhere, BluePrintReadWrite)
 	float PlayerHP = 100.0;
 
 	UPROPERTY(EditAnywhere, BluePrintReadWrite)
-	bool PlayerDie = false;
+	bool IsDie = false;
 
 	// getters and setters for Player HP
 public:
@@ -204,5 +198,7 @@ private:
 	FTimerHandle RewindTimerHandle;
 	// Rewind Cooldown function
 	void RewindCooldown();
+
+	void PlayerDie();
 
 };
