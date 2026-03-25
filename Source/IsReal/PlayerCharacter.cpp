d5026cd6371032a3241aa9f34cabef1e0af11b41
tjdwns1314 <tjdwns1314@gmail.com>
2026-03-25 19:28:59 +0900

    [Commit] 애니메이션 여러 오류 수정

--- Source/IsReal/PlayerCharacter.cpp
+++ Source/IsReal/PlayerCharacter.cpp
@@ -338,6 +338,7 @@ void APlayerCharacter::DoShootingEnd()
 void APlayerCharacter::Reload(const FInputActionValue& inputValue)
 {	
 	if (IsShooting) return;
+	if (IsRolling) return;
 	if (CurrentWeapon && IsHasGun) {
 		CurrentWeapon->WeaponReload();
 	}
@@ -404,6 +405,10 @@ void APlayerCharacter::Playerknockback() // delete
 
 	GetWorld()->GetTimerManager().SetTimer(KnockbackTimer, this, &APlayerCharacter::KnockbackEnd, 1.0f, false);
 
+	if (isAiming)
+	{
+		DoAimEnd();
+	}
 	if (IsLookTimer) { //시계를 보고 있었으면 시계를 끄기 
 		if (AnimInstance && ToggleClockMontage)
 		{
